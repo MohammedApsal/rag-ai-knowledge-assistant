9a99abcb325e634d9e927c9e488e0ec0805a0a9d
mohammed apsal <apsal1000@gmail.com>
2026-08-15 20:56:44 +0530

    Allow deployed frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",
+        "https://rag-ai-knowledge-assistant-frontend.onrender.com"
     ],
     allow_credentials=False,
     allow_methods=["*"],
